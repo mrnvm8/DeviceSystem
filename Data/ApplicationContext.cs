4a50566ff697ae3302678e95b3e9bdcad75fd9b6
mrnvm8 <mrnvm8@gmail.com>
2023-11-01 12:35:18 +0200

    ApplicationContext updates

--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -11,59 +11,80 @@ namespace DeviceSystem.Data
       modelBuilder.Entity<Department>()
         .HasOne<Office>(o => o.Offices)
         .WithMany(d => d.Departments)
-        .HasForeignKey(o => o.OfficeId);
+        .HasForeignKey(o => o.OfficeId)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<Employee>()
          .HasOne<Department>(p => p.Department)
          .WithMany(p => p.Employees)
-         .HasForeignKey(p => p.DepartmentId);
+         .HasForeignKey(p => p.DepartmentId)
+         .IsRequired()
+         .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<Employee>()
           .HasOne<Person>(p => p.Person)
           .WithMany(p => p.Employees)
-          .HasForeignKey(p => p.PersonId);
+          .HasForeignKey(p => p.PersonId)
+          .IsRequired()
+          .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<Device>()
           .HasOne<Department>(e => e.Department)
           .WithMany(u => u.Devices)
-          .HasForeignKey(e => e.DepartmentId);
+          .HasForeignKey(e => e.DepartmentId)
+          .IsRequired()
+          .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<Device>()
           .HasOne<DeviceType>(e => e.DeviceType)
           .WithMany(u => u.Devices)
-          .HasForeignKey(e => e.DeviceTypeId);
+          .HasForeignKey(e => e.DeviceTypeId)
+          .IsRequired()
+          .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<DeviceLoans>()
         .HasOne<Device>(e => e.Device)
         .WithMany(u => u.DevicesLoans)
-        .HasForeignKey(e => e.DeviceId);
+        .HasForeignKey(e => e.DeviceId)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<DeviceLoans>()
         .HasOne<Employee>(e => e.Employee)
         .WithMany(u => u.DevicesLoans)
-        .HasForeignKey(e => e.EmployeeId);
+        .HasForeignKey(e => e.EmployeeId)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<User>()
           .HasOne<Employee>(e => e.Employee)
           .WithMany(u => u.Users)
-          .HasForeignKey(e => e.EmployeeId);
+          .HasForeignKey(e => e.EmployeeId)
+          .IsRequired()
+          .OnDelete(DeleteBehavior.Cascade);
 
       modelBuilder.Entity<DeviceLoans>()
         .HasOne<User>(e => e.User)
         .WithMany(u => u.DevicesLoans)
-        .HasForeignKey(e => e.UserId);
+        .HasForeignKey(e => e.UserId)
+        .IsRequired()
+        .OnDelete(DeleteBehavior.Cascade);
 
-      //!Foreign key Relationship between Ticket -> Login tables
       modelBuilder.Entity<Ticket>()
           .HasOne<User>(p => p.Users)
           .WithMany(d => d.Tickets)
-          .HasForeignKey(f => f.UserId);
+          .HasForeignKey(f => f.UserId)
+          .IsRequired()
+          .OnDelete(DeleteBehavior.Cascade);
 
-      //!Foreign key Relationship between Ticket -> Login tables
+     
       modelBuilder.Entity<Ticket>()
           .HasOne<Device>(p => p.Devices)
           .WithMany(d => d.Tickets)
-          .HasForeignKey(f => f.DeviceId);
+          .HasForeignKey(f => f.DeviceId)
+          .IsRequired()
+          .OnDelete(DeleteBehavior.Cascade);
     }
 
     public DbSet<User>? Users { get; set; }
